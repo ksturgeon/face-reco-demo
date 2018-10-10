6a64a9f19336150fabe452d7246609632b4c04ab
Kevin Sturgeon <ksturgeon@maprtech.com>
2018-10-10 16:45:57 -0400

    added more setup stuff and reader shell

--- install-kafka-client.sh
+++ install-kafka-client.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sudo apt-get install gcc -y
+sudo apt-get install python-devel -y
+sudo apt-get install python-pip -y
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mapr/lib
 
 sudo pip install --upgrade --force-reinstall --global-option=build_ext --global-option="--library-dirs=/opt/mapr/lib" --global-option="--include-dirs=/opt/mapr/include/" http://package.mapr.com/releases/MEP/MEP-5.0/mac/mapr-streams-python-0.11.0.tar.gz
 
-sudo pip install opencv-python mxnet-cu90 flask graphviz easydict scipy tensorflow sklearn scikit-image
+sudo pip install opencv-python mxnet-cu90 flask graphviz easydict scipy tensorflow sklearn scikit-image numpy
