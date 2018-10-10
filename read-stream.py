57baf675810a5566b13147e1a1090845b2857af3
Kevin Sturgeon <ksturgeon@maprtech.com>
2018-10-10 14:59:06 -0400

    added stream reader

---
+++ read-stream.py
@@ -0,0 +1,16 @@
+from confluent_kafka import Consumer, KafkaError
+import datetime
+c = Consumer({'group.id': 'mygroup',
+              'default.topic.config': {'auto.offset.reset': 'earliest'}})
+c.subscribe(['/demo-stream:topic1'])
+running = True
+while running:
+  msg = c.poll(timeout=1.0)
+  if msg is None: continue
+  if not msg.error():
+    print('\n')
+    print('*****Received message at: '+ str(datetime.datetime.fromtimestamp(msg.timestamp()[1] / 1e3)) + ' : ' +  msg.value().decode('utf-8'))
+  elif msg.error().code() != KafkaError._PARTITION_EOF:
+    print(msg.error())
+    running = False
+c.close()
