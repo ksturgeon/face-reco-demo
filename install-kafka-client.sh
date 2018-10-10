8221062b28212e21f925d5e07d1a183a60bd1ef9
Kevin Sturgeon <ksturgeon@maprtech.com>
2018-10-10 15:20:45 -0400

    fixed client setup

--- install-kafka-client.sh
+++ install-kafka-client.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install gcc
+sudo apt-get install gcc -y
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mapr/lib
 
 sudo pip install --upgrade --force-reinstall --global-option=build_ext --global-option="--library-dirs=/opt/mapr/lib" --global-option="--include-dirs=/opt/mapr/include/" http://package.mapr.com/releases/MEP/MEP-5.0/mac/mapr-streams-python-0.11.0.tar.gz
 
-sudo pip install numpy,cv2
+sudo pip install numpy
+sudo pip install opencv-python
